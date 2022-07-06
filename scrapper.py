81d2c699e3def8e394bd7b0e4c4fb54e5fc7f53f
akshajdwivedula <62357669+akshajdwivedula@users.noreply.github.com>
2022-07-06 14:40:24 -0700

    Update scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -3,19 +3,18 @@ import webbrowser
 import os
 import zipfile
 import re
+import docx2txt
 
 
 # webbrowser.open('https://www.legalmatch.com/find-lawyers.html')
 global Options
 # setting up options and creating file reader to read off a daily script
 Options = [287, 286, 285, 291, 294, 64, 289, 292]
-import os
-file_ = 'Dailyscript.docx'
 
-for root, dirs, files in os.walk('C:/'):
-    for name in files:
-        if file_ in name:
-            print (os.path.abspath(os.path.join(root, name)))
+os.chdir(r'C:\\Users\\adwiv\\OneDrive - The University of Chicago\\Research\\George\\')
 
-# y = "https://www.legalmatch.com/post-case/subcategory?supCatIds="+str(x)+"&combo_loc=false"
-# webbrowser.open(y)
+result = docx2txt.process("DailyScript.docx").lower()
+if "family" in result:
+    x = Options[0]
+    y = "https://www.legalmatch.com/post-case/subcategory?supCatIds="+str(x)+"&combo_loc=false"
+    webbrowser.open(y)
